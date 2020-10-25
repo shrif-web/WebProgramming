8488e3d8a6a83c12a2be0a1f2f3c8b1d7bc4ec95
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-10-25 17:02:46 +0330

    added node js file (not completed)

---
+++ HW1/nodejs/sha256.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require('fs');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const hostname = "0.0.0.0";
+const port = 8080;
+
+app.post("/nodejs/sha256", (request, response) => {
+    console.log(`Request came: ${request.url}`);
+
+    try{
+        let parts = request.url.substring(5).split('/')
+        let result = Number(parts[0]) + Number(parts[1])
+        response.json({sum: result})
+        response.end();
+        console.log(`Response: 200 ${request.url}`);
+        return;
+    }catch(e){
+        console.log(e);
+    }
+});
+
+fs.readFile('./front.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    const server = http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`server running at http://${hostname}:${port}/`);
+    });
+});
