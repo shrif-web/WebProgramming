69b2896e5413eb4123d96d995e411c9b14469f6f
majid taherkhani <majidtaherkhani555@gmail.com>
2020-10-29 00:36:26 +0330

    get line

--- HW1/go/sha256.go
+++ HW1/go/sha256.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 type SumRequestBody struct {
@@ -15,6 +17,14 @@ type SumRequestBody struct {
 
 type SumResponseBody struct {
 	Sum int `json:"sum"`
+}
+
+type LineNumber struct {
+	Line int `json:"line"`
+}
+
+type LineContent struct {
+	Line string `json:"line"`
 }
 
 func sumRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,9 +45,47 @@ func sumRequestHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(sum)
 }
 
+func line(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
+		return
+	}
+	reqBodyJson, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lineN := LineNumber{}
+	json.Unmarshal(reqBodyJson, &lineN)
+
+	fileIO, err := os.OpenFile("file.txt", os.O_RDWR, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer fileIO.Close()
+	rawBytes, err := ioutil.ReadAll(fileIO)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(rawBytes), "\n")
+	w.Header().Set("Content-Type", "application/json")
+	for i, line := range lines {
+		if i == lineN.Line-1 {
+			if i != 100 {
+				runes := []rune(line)
+				json.NewEncoder(w).Encode(LineContent{string(runes[0 : len(line)-1])})
+			} else {
+				json.NewEncoder(w).Encode(LineContent{line})
+			}
+		}
+	}
+}
+
 func main() {
+
 	var port = ":8000"
 	fmt.Println("Starting Go Server At localhost", port)
 	http.HandleFunc("/sha256", sumRequestHandler)
+	http.HandleFunc("/write", line)
 	http.ListenAndServe(port, nil)
 }
