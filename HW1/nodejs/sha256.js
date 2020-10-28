1690c84283ec2b9819cb76faa079eb84b5c36ff8
fatteme <fatt3me@gmail.com>
2020-10-28 14:48:43 +0330

    add package.json to nodejs

--- HW1/nodejs/sha256.js
+++ HW1/nodejs/sha256.js
@@ -5,32 +5,32 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-const hostname = "0.0.0.0";
+const hostname = "127.0.0.1";
 const port = 8080;
 
 app.post("/nodejs/sha256", (request, response) => {
     console.log(`Request came: ${request.url}`);
 
-    try{
+    try {
         let parts = request.url.substring(5).split('/')
         let result = Number(parts[0]) + Number(parts[1])
-        response.json({sum: result})
+        response.json({ sum: result })
         response.end();
         console.log(`Response: 200 ${request.url}`);
         return;
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 });
 
-fs.readFile('./front.html', function (err, html) {
+fs.readFile('../front/front.html', function (err, html) {
     if (err) {
-        throw err; 
-    }       
-    const server = http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+        throw err;
+    }
+    const server = http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
     });
 
     server.listen(port, hostname, () => {
