7dc98a934424e0c6b0fa048bae2858d83d52c9e1
fatteme <fatt3me@gmail.com>
2020-10-28 22:29:02 +0330

    initial version of nginx

--- HW1/front/js/script.js
+++ HW1/front/js/script.js
@@ -1,21 +1,23 @@
+//set your centos ip in here
+const HOST_API = "http://172.20.10.6"
 const FAIL = "Sorry! We couldnt make the request"
-function calculateSumNodeJs() {
+function calculateSum(serverType) {
     const firstNumber = document.getElementById("input1").value;
     const secondNumber = document.getElementById("input2").value;
-    sendRequest('POST', "http://localhost:8080/", { firstNumber, secondNumber })
+    sendRequest('POST', `${HOST_API}/${serverType}/sha256`, { firstNumber, secondNumber })
         .then(result => {
             alert(result ? result.sum : FAIL);
         })
 }
 
-function calculateSumGo() {
-    const firstNumber = document.getElementById("input1").value;
-    const secondNumber = document.getElementById("input2").value;
-    sendRequest('POST', "http://localhost:8000/", { firstNumber, secondNumber })
+function getLine(serverType) {
+    const lineNumber = document.getElementById("lineNumber").value;
+    sendRequest('GET', `${HOST_API}/${serverType}/write`, { lineNumber })
         .then(result => {
-            alert(result ? result.sum : FAIL);
+            alert(result ? result : FAIL);
         })
 }
+
 
 function sendRequest(type, url, payload) {
     var myHeaders = new Headers();
