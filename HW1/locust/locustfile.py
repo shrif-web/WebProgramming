5ddc2d902c81a2b2e2b7806623be1bbd15914996
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-10-29 20:18:16 +0330

    changed locust

--- HW1/locust/locustfile.py
+++ HW1/locust/locustfile.py
@@ -6,17 +6,12 @@ class QuickstartUser(HttpUser):
 
     @task
     def sum(self):
-        result = self.client.post("/sha256", 
+        self.client.post("/sha256", 
         {
             "firstNumber": 1,
             "secondNumber": 2
         })
-        print("result text:", result.sum)
         
     @task
     def write(self):
-        result = self.client.get("/write", 
-        {
-            "lineNumber": 3
-        })
-        print("result text:", result)
+        self.client.get("/write?lineNumber=3")
