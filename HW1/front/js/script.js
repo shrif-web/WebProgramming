599ce0c2ec7558a05da6f2e6c909d04e348e4a4d
fatteme <fatt3me@gmail.com>
2020-10-29 18:42:35 +0330

    remove host from nodejs

--- HW1/front/js/script.js
+++ HW1/front/js/script.js
@@ -34,7 +34,7 @@ function getLine(serverType) {
                 showError();
                 return;
             }
-            line.innerText = result;
+            line.innerText = result.line;
         })
 }
 
