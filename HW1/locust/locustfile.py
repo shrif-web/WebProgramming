84f4cc290c0e1ee0aa9d786b0313f98326f5d001
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-10-29 20:33:54 +0330

    locust file updated

--- HW1/locust/locustfile.py
+++ HW1/locust/locustfile.py
@@ -5,13 +5,25 @@ class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
     @task
-    def sum(self):
-        self.client.post("/sha256", 
+    def sumNodejs(self):
+        self.client.post("nodejs/sha256", 
         {
             "firstNumber": 1,
             "secondNumber": 2
         })
+
+    @task
+    def sumGo(self):
+        self.client.post("go/sha256", 
+        {
+            "firstNumber": 3,
+            "secondNumber": 4
+        })
         
     @task
-    def write(self):
-        self.client.get("/write?lineNumber=3")
+    def writeNodejs(self):
+        self.client.get("nodejs/write?lineNumber=5")
+
+    @task
+    def writeGo(self):
+        self.client.get("go/write?lineNumber=6")
