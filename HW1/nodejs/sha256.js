88ea015d92386ec3270a7c09c90e2d16be991969
fatteme <fatt3me@gmail.com>
2020-10-29 13:32:42 +0330

    make nodejs file better

--- HW1/nodejs/sha256.js
+++ HW1/nodejs/sha256.js
@@ -9,31 +9,27 @@ app.use(cors());
 app.use(express.json());
 
 const hostname = "127.0.0.1";
-const port = 8000;
+const port = 8080;
 
-app.post("/nodejs/sha256", (request, response) => {
-    console.log("POST /nodejs/sha256");
+app.post("/sha256", (request, response) => {
+    console.log("POST /sha256");
     const firstNumber = request.body.firstNumber;
     const secondNumber = request.body.secondNumber;
-    let sum;
-    if (typeof firstNumber === 'number' && typeof secondNumber === 'number')
-    {
-        sum = firstNumber + secondNumber;
-        sum = crypto.createHash('sha256').update(sum).digest('hex');
+    if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
+        const sumRaw = firstNumber + secondNumber;
+        const sum = crypto.createHash('sha256').update(sumRaw).digest('hex');
+        response.json({ sum })
+        response.end();
     }
-    else
-    {
+    else {
         return response.status(400).end('Inputs must be numbers');
     }
-    response.json({ sum })
-    response.end();
 });
 
-app.get("/nodejs/write", (request, response) => {
-    console.log("GET /nodejs/write");
+app.get("/write", (request, response) => {
+    console.log("GET /write");
     const lineNumber = req.body.lineNumber;
-    if (typeof lineNumber !== 'number' || lineNumber < 1 || lineNumber > 100)
-    {
+    if (typeof lineNumber !== 'number' || lineNumber < 1 || lineNumber > 100) {
         return response.status(400).end('Inputs must be numbers');
     }
 
@@ -46,17 +42,10 @@ app.get("/nodejs/write", (request, response) => {
     response.json({ result })
 });
 
-fs.readFile('../front/front.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    const server = http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    });
-
-    server.listen(port, hostname, () => {
-        console.log(`server running at http://${hostname}:${port}/`);
-    });
+const server = http.createServer(function (request, response) {
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.end();
+});
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}/`);
 });
